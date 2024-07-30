8de404c20c19b4b25e30274dd4158765fa2a2d7b
Chelsea Jarvis <chelseajarvis3301@icloud.com>
2024-07-29 21:08:17 -0600

    thought controllers

--- controllers/thoughtContoller.js
+++ controllers/thoughtContoller.js
@@ -1,1 +1,20 @@
 ﻿const Thought = require('../models/Thought');
+
+// Get a single thought by ID
+// Create a new thought
+// Update a thought by ID
+// Delete a thought by ID
+// Create a reaction for a thought
+// Delete a reaction by ID
+
+
+
+// Get all thoughts
+const getAllThoughts = async (req, res) => {
+    try {
+        const thoughts = await Thought.find();
+        res.status(200).json(thoughts);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
