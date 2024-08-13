e71ac6750dd3b1d795bcb0fb6d65b1d8ee196474
Chelsea Jarvis <chelseajarvis3301@icloud.com>
2024-08-12 22:38:54 -0600

    added functions to set up a user and track errors

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,11 +2,46 @@
 const { User, Thought } = require('../models');
 
 module.exports = {
-    // get all users
-    // get one user
+  // Get all Users
+  async getUsers(req, res) {
+    try {
+      const users = await User.find();
+      res.json(users);
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ message: "Something went wrong!, Please try again later." });
+    }
+  },
+
+    // Get Single user
+    async getSingleUser(req, res) {
+        try {
+          const user = await User.findOne({ _id: req.params.userId })
+            .select("-__v")
+            .populate("thoughts")
+            .populate("friends");
+    
+          if (!user) {
+            return res.status(404).json({ message: "No user with that id" });
+          }
+    
+          res.json(user);
+        } catch (err) {
+          console.error(err);
+          return res.status(500).json({ message: "Something went wrong!, No user found." });
+        }
+      },
+    
     // create user
+    
     // update user
+    
     // delete user
+    
     // add friend
+    
+    
     // delete friend
+
+
 };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,7 +1,7 @@
 ﻿const router = require('express').Router();
 const userController = require('../../controllers/userController');
 const {
-    getUsers,
+    getUsers, 
     getSingleUser,
     createUser,
     updateUser,
